09a97c4713aa8d4c22278c79613dc60a0ed1f029
yaolinxia <18860976931@163.com>
2019-12-17 20:16:29 +0800

    数据集更新sighan

--- pycorrector/seq2seq_attention/config.py
+++ pycorrector/seq2seq_attention/config.py
@@ -13,7 +13,7 @@ raw_train_paths = [
     os.path.join(pwd_path, '../data/cn/CGED/CGED16_HSK_TrainingSet.xml'),
     # os.path.join(pwd_path, '../data/cn/CGED/sample_HSK_TrainingSet.xml'),
 ]
-
+# 原始output
 output_dir = os.path.join(pwd_path, 'output')
 # Training data path.
 train_path = os.path.join(output_dir, 'train.txt')
@@ -25,6 +25,14 @@ result_path = os.path.join(output_dir, "result.txt")
 save_vocab_path = os.path.join(output_dir, 'vocab.txt')
 attn_model_path = os.path.join(output_dir, 'attn_model.weight')
 
+# 加入台湾数据集，设置新的输出目录
+output_sighan_dir = os.path.join(pwd_path, 'output_sighan')
+# sighan_train path
+train_sighan_path = os.path.join(output_sighan_dir, 'sighan_output_train.txt')
+# sighan_test path
+test_sighan_path = os.path.join(output_sighan_dir, "sighan_output_test.txt")
+result_path = os.path.join(output_sighan_dir, "result_sighan.txt")
+
 # config
 batch_size = 64
 epochs = 40
@@ -35,3 +43,5 @@ use_gpu = False
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
+if not os.path.exists(output_sighan_dir):
+    os.makedirs(output_sighan_dir)
